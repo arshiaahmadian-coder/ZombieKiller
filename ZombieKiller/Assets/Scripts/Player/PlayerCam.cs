51af2785828e8fa80705f9be30f8daf540260b40
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2025-11-27 21:15:50 +0330

    adding assets, level design, ready to go

---
+++ ZombieKiller/Assets/Scripts/Player/PlayerCam.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float senseY;
+    public float senseX;
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    void Start() {
+        Cursor.vi
+    }
+}
