bb987a606407d86fe99f89da5cc58f05d859d368
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2025-11-27 23:37:05 +0330

    adding playr movement

--- ZombieKiller/Assets/Scripts/Player/PlayerCam.cs
+++ ZombieKiller/Assets/Scripts/Player/PlayerCam.cs
@@ -9,7 +9,20 @@ public class PlayerCam : MonoBehaviour
     float xRotation;
     float yRotation;
 
-    void Start() {
-        Cursor.vi
+    private void Start() {
+        // Cursor.visible = false;
+    }
+
+    private void Update() {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senseX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senseY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
---
+++ ZombieKiller/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("movement")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] Transform orientation;
+    [SerializeField] float groundDamping;
+
+    [SerializeField] float jumpForce;
+    [SerializeField] float jumpCooldown;
+    [SerializeField] float airMultiplier;
+    bool readyToJump = true;
+
+    [Header("Ground check")]
+    [SerializeField] float playerHeight;
+    [SerializeField] LayerMask GroundLayer;
+    bool grounded;
+
+    [Header("Key Binds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, GroundLayer);
+        myInput();
+        SpeedControl();
+
+        // handle drag
+        if(grounded) rb.linearDamping = groundDamping;
+        else rb.linearDamping = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void myInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calc move direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+        else
+            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
