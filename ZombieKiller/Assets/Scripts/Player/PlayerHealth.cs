33c6bfbb04a0805470f4e5563b57f1bad5801b7b
arshiaahmadian-coder <arshiaahmadiancoder@gmail.com>
2025-11-29 14:10:28 +0330

    zombies path finding, attack and run animation, player heath system

---
+++ ZombieKiller/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float maxHealth;
+    [SerializeField] TMP_Text healthText;
+    private float currentHealth;
+
+    public static PlayerHealth instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthText.text = currentHealth.ToString();
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        healthText.text = currentHealth.ToString();
+        // TODO: play hurt sound
+        if(currentHealth <= 0f)
+        {
+            // TODO: Save Kill record, show menu;
+            print("GameOver");
+        }
+    }
+}
--- ZombieKiller/Assets/Scripts/Zombies/ZombiePathFinding.cs
+++ ZombieKiller/Assets/Scripts/Zombies/ZombiePathFinding.cs
@@ -3,17 +3,55 @@ using UnityEngine.AI;
 
 public class ZombiePathFinding : MonoBehaviour
 {
+    [Header("Attack")]
+    [SerializeField] float AttackRange;
+    [SerializeField] float AttackDamage;
+    [SerializeField] float baseAttackTime;
+
+    private bool readyToAttack = true;
     private Transform target;
     private NavMeshAgent agent;
+    private Animator animator;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        target = FindAnyObjectByType<PlayerMovement>().transform;
+        target = FindFirstObjectByType<PlayerMovement>().transform;
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        agent.SetDestination(target.position);
+        if (!agent.isStopped) agent.SetDestination(target.position);
+        float remainingDist = agent.remainingDistance;
+
+        // Attack
+        if(readyToAttack && remainingDist <= AttackRange) Attack();
+    }
+
+    private void Attack()
+    {
+        // deal damage
+        // trigger animation
+        animator.SetTrigger("Attack");
+        // reset flags
+        readyToAttack = false;
+        Invoke(nameof(ResetAttackTime), baseAttackTime);
+        Invoke(nameof(DealDamage), baseAttackTime * 0.35f);
+    }
+
+    private void ResetAttackTime()
+    {
+        readyToAttack = true;
+    }
+
+    private void DealDamage()
+    {
+        float remainingDist = agent.remainingDistance;
+        if(remainingDist <= AttackRange)
+        {
+            // decrease player health
+            PlayerHealth.instance.TakeDamage(AttackDamage);
+        }
     }
 }
